937e896dcf148fbeb66953679f0bc9504ffd93ac
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-07-29 22:08:34 +0530

    apollo/client added in Provider added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
+    <ApolloProvider  client ={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +43,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
