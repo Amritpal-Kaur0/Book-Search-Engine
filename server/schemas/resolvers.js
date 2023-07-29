f3cfd4741389a7d28a683e94dfa5f1a9b1ab9611
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-07-29 17:39:42 +0530

    typedefs inputbook and auth added,bugs fixed in typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,13 +2,61 @@ const { User } = require('../models');
 const bookSchema = require('../models/Book');
 
 const resolvers = {
-    Query: {
-      Books: async () => {
-        return User.find()
-      },
-  
-      thought: async (parent, { bookId }) => {
-        return bookSchema.findOne({ _id: bookId });
-      },
+  Query: {
+    // Resolver to get a user by ID
+    getUser: async (_, { id }) => {
+      try {
+        const user = await User.findById(id);
+        return user;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
     },
-}
+  },
+    Mutation:{
+// Resolver for creating a new user
+createUser: async (_, { input }) => {
+  try {
+    const user = await User.create(input);
+    return { success: true, message: 'User created successfully', user };
+  } catch (error) {
+    return { success: false, message: error.message, user: null };
+  }
+},
+
+// Resolver for updating an existing user
+updateUser: async (_, { id, input }) => {
+  try {
+    const user = await User.findByIdAndUpdate(id, input, { new: true });
+    if (!user) {
+      return { success: false, message: 'User not found', user: null };
+    }
+    return { success: true, message: 'User updated successfully', user };
+  } catch (error) {
+    return { success: false, message: error.message, user: null };
+  }
+},
+
+// Resolver for deleting a user
+deleteUser: async (_, { id }) => {
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return { success: false, message: 'User not found', user: null };
+    }
+    return { success: true, message: 'User deleted successfully', user };
+  } catch (error) {
+    return { success: false, message: error.message, user: null };
+  }
+},
+// You can add more mutation resolvers here as needed
+},
+User: {
+// Resolver for the `savedBooks` field, which should return an array of books
+savedBooks: (parent) => parent.savedBooks || [],
+
+// Resolver for the `bookCount` field, which should return the length of savedBooks array
+bookCount: (parent) => parent.savedBooks.length,
+},
+};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,26 +1,46 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-
-const typeDefs = gql`
-  type bookSchema {
-    _id: ID
-    authors: String
+export const typeDefs = gql`
+  type Book {
+    bookid: ID!
+    authors: [String]
     description: String
+    image: String
+    link: String
+    title: String
+  }
+  input BookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 
   type User {
     _id: ID
-    username: String
-    email: String
-    password:String
-    savedBooks:[bookSchema]
+    username: String!
+    email: String!
+    savedBooks: [Book]
+    bookCount: Int!
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    Books: [Book]!
-    book(bookId: ID!): Book
+   me:User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
   }
 
-  `;
-  
-  module.exports = typeDefs;
+`;
+
+// module.exports = typeDefs;
