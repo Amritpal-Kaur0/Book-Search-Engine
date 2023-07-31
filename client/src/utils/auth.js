6feed0b22a10b9ef27aeb781707150c52e6ad92e
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-07-31 16:47:20 +0530

    mutation and queries added

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -7,14 +7,14 @@ class AuthService {
   getProfile() {
     return decode(this.getToken());
   }
-
+  
   // check if user's logged in
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token); // handwaiving here
   }
-
+  
   // check if token is expired
   isTokenExpired(token) {
     try {
@@ -37,13 +37,15 @@ class AuthService {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
-
+  
   logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
     // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
-}
+};
+
 
 export default new AuthService();
+
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,24 +1,84 @@
 import gql from "graphql-tag";
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
+mutation login(
+  $email: String!
+  $password: String!
+) {
+  login(
+    email: $email
+    password: $password
+  ) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+
+`;
+export const ADD_USER = gql`
+mutation addUser(
+  $username: String!
+  $email: String!
+  $password: String!
+) {
+  addUser(
+    username: $username
+    email: $email
+    password: $password
+  ) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+}
+`;
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: InputBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
       }
     }
   }
 `;
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
       }
-    }y
+    }
   }
-`;
+  `
