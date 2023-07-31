bccce41d89fa0f0e3ee7631de4a27d2e6f390cc8
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-07-31 23:26:52 +0530

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect("mongodb+srv://Amrit:Hello12345@cluster0.jxabhpp.mongodb.net/googlebooks" || 'mongodb://127.0.0.1:27017/googlebooks');
 
 
 module.exports = mongoose.connection;
